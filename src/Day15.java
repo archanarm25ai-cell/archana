c348479e16f27904ddad2abb1660766a6e8fbced
archana <archanarm25ai@vetias.ac.in>
2026-06-23 11:22:46 +0530

    pickorder

--- src/Day15.java
+++ src/Day15.java
@@ -3,11 +3,11 @@ import java.time.Duration;
 public class Day15 {
     static void main() throws InterruptedException {
         Runnable chefTask = () -> {
-            String[] items = {"Burger","pizza","chicken briyani"};
+            String[] items = {"Burger","pizza","chicken briyani" ,"popcorn","barbeque chicken","prawn fry"};
             for(int i=0;i<items.length;i++){
                 System.out.println("cooking Item:"+ items[i]);
                 try{
-                    Thread.sleep(10000);
+                    Thread.sleep(2000);
                 } catch (InterruptedException e){
                     throw new RuntimeException(e);
                 }
